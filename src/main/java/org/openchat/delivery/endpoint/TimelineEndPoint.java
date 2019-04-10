0c72ae2a8eb85c15bf6e59721055b504c19469c8
Daniele Megna <megna.dany@gmail.com>
2019-04-10 14:44:03 +0200

    Another little step towards usecase extraction

--- src/main/java/org/openchat/delivery/endpoint/TimelineEndPoint.java
+++ src/main/java/org/openchat/delivery/endpoint/TimelineEndPoint.java
@@ -1,5 +1,6 @@
 package org.openchat.delivery.endpoint;
 
+import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonObject;
 import org.openchat.delivery.HexagonalRequest;
 import org.openchat.delivery.HexagonalResponse;
@@ -14,20 +15,30 @@ public class TimelineEndPoint implements EndPoint {
     }
 
     private HexagonalResponse runUseCase(HexagonalRequest request) {
-        Post newPost = createPost(request);
-        return new HexagonalResponse(201, "application/json", serializePost(newPost));
+        Post toBeCreated = parsePostFrom(request);
+        Post newPost = createPost(toBeCreated);
+        return new HexagonalResponse(201, "application/json", serialize(newPost));
     }
 
-    private Post createPost(HexagonalRequest request) {
+    private Post parsePostFrom(HexagonalRequest request) {
+        JsonObject postJson = Json.parse(request.body).asObject();
         return new Post(
-            UUID.randomUUID().toString(),
             request.params.get(":userid"),
-            "Hello, I'm Alice",
+            postJson.getString("text", ""),
             "2018-01-10T11:30:00Z"
         );
     }
 
-    private String serializePost(Post newPost) {
+    private Post createPost(Post toBeCreated) {
+        return new Post(
+            UUID.randomUUID().toString(),
+            toBeCreated.userId,
+            toBeCreated.text,
+            toBeCreated.dateTime
+        );
+    }
+
+    private String serialize(Post newPost) {
         return new JsonObject()
             .add("postId", newPost.id)
             .add("userId", newPost.userId)
--- src/main/java/org/openchat/domain/entity/Post.java
+++ src/main/java/org/openchat/domain/entity/Post.java
@@ -6,6 +6,10 @@ public class Post {
     public final String text;
     public final String dateTime;
 
+    public Post(String userId, String text, String dateTime) {
+        this(null, userId, text, dateTime);
+    }
+
     public Post(String id, String userId, String text, String dateTime) {
         this.id = id;
         this.userId = userId;
